f12367b78e33d27e84eb4ebfaa95b7e0ad3626aa
simjeongsoo <simking95@naver.com>
2022-06-07 10:39:16 +0900

    springboot webservice init - buildgradle, main class

---
+++ src/main/java/springbootweb/Application.java
@@ -0,0 +1,14 @@
+package springbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// main class
+// @SpringBootApplication 를 통해 스프링부트 자동생성 , Bean 읽기 생성 자동설정
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        //SpringApplication.run 으로 내장 WAS 실행
+        SpringApplication.run(Application.class, args);
+    }
+}
