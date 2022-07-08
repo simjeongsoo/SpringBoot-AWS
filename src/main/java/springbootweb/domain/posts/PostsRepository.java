edcee3d9e64d63bdcef575b05871f3e0e1511136
simjeongsoo <simking95@naver.com>
2022-07-08 13:34:36 +0900

    JPA 2.rename JpaRepository -> PostsRepository

--- src/main/java/springbootweb/domain/posts/PostsRepository.java
+++ src/main/java/springbootweb/domain/posts/PostsRepository.java
@@ -2,7 +2,7 @@ package springbootweb.domain.posts;
 
 // Posts 클래스로 Database 를 접근하게 해줄 인터페이스 (MyBatis 에서 DAO)
 // extends JpaRepository<Entity 클래스, PK 타입> -> 상속시 기본적인 CRUD 메소드 자동생성
-// Entity class 와 Entity Repository class 는 같은 패키지에 위치 
-public interface JpaRepository extends org.springframework.data.jpa.repository.JpaRepository<Posts,Long> {
+// Entity class 와 Entity Repository class 는 같은 패키지에 위치
+public interface PostsRepository extends org.springframework.data.jpa.repository.JpaRepository<Posts,Long> {
 
 }
