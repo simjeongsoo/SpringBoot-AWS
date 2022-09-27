28f13c9f16c7e776a97b4e9f5ae99ecbd7120a8e
simjeongsoo <simking95@naver.com>
2022-09-27 18:39:40 +0900

    조회 기능 로직 추가

--- src/main/java/springbootweb/service/posts/PostsService.java
+++ src/main/java/springbootweb/service/posts/PostsService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import springbootweb.domain.posts.Posts;
 import springbootweb.domain.posts.PostsRepository;
+import springbootweb.web.dto.PostsResponseDto;
 import springbootweb.web.dto.PostsSaveRequestDto;
 import springbootweb.web.dto.PostsUpdateRequestDto;
 
@@ -15,16 +16,24 @@ public class PostsService {
 
     // JPA를 통한 모든 데이터 변경은 트랜잭션 안에서 실행해야 한다.
     @Transactional // for jpa
-    public Long save(PostsSaveRequestDto requestDto) {
+    public Long save(PostsSaveRequestDto requestDto) { // 등록 로직
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
     @Transactional
-    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+    public Long update(Long id, PostsUpdateRequestDto requestDto) { // 수정 로직
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() ->
                         new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
+    }
+
+    public PostsResponseDto findById(Long id) { // 조회 로직
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()->
+                        new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+
     }
 }
