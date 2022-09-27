53511f525d70fcacc4b0c8a200ec6193969793a5
simjeongsoo <simking95@naver.com>
2022-09-27 18:07:36 +0900

    게시글 수정 기능을 위한 Controller에 수정 api 추가

--- src/main/java/springbootweb/web/PostsApiController.java
+++ src/main/java/springbootweb/web/PostsApiController.java
@@ -1,19 +1,25 @@
 package springbootweb.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springbootweb.service.posts.PostsService;
 import springbootweb.web.dto.PostsSaveRequestDto;
+import springbootweb.web.dto.PostsUpdateRequestDto;
 
 @RequiredArgsConstructor // 생성자 주입(lombok)
 @RestController
 public class PostsApiController {
     private final PostsService postsService;
 
-    @PostMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts") // 등록 api
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
+
+    @PutMapping("/api/v1/posts/{id}") // 수정 api 
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    // 조회
 }
