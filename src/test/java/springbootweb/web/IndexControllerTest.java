a6449aa5454e592b3e524627d9366c15466de21b
simjeongsoo <simking95@naver.com>
2022-09-28 20:06:52 +0900

    실제로 URL 호출시 페이지의 내용이 제대로 호출 되는지에 대한 테스트

---
+++ src/test/java/springbootweb/web/IndexControllerTest.java
@@ -0,0 +1,34 @@
+package springbootweb.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() throws Exception {
+        //given
+
+
+        //when
+        // getForObject(url, responseType,urlVariables)
+        // 지정된 URL에서 GET을 수행하여 표현을 검색합니다. 응답(있는 경우)이 변환되어 반환됩니다.
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+
+    }
+
+}
