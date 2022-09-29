d58736234279a338ed99f63809a1aa3011416fe3
simjeongsoo <simking95@naver.com>
2022-09-29 19:49:53 +0900

    전체 조회 Service 로직 추가

--- src/main/java/springbootweb/service/posts/PostsService.java
+++ src/main/java/springbootweb/service/posts/PostsService.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import springbootweb.domain.posts.Posts;
 import springbootweb.domain.posts.PostsRepository;
+import springbootweb.web.dto.PostsListResponseDto;
 import springbootweb.web.dto.PostsResponseDto;
 import springbootweb.web.dto.PostsSaveRequestDto;
 import springbootweb.web.dto.PostsUpdateRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                         new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
 
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() { // 전체 글 조회 로직
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
