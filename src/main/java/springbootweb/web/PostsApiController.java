3f615fe043436983129c864c96ea95aca553a483
simjeongsoo <simking95@naver.com>
2022-09-29 20:01:34 +0900

    [삭제 API 구현] - PostsApiController

--- src/main/java/springbootweb/web/PostsApiController.java
+++ src/main/java/springbootweb/web/PostsApiController.java
@@ -22,8 +22,14 @@ public class PostsApiController {
         return postsService.update(id, requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id}")// 조회
+    @GetMapping("/api/v1/posts/{id}")// 조회 api
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}") // 삭제 api
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 }
