0b039d062d9062e4fd96cad6be3d87078c081b0b
simjeongsoo <simking95@naver.com>
2022-09-30 20:17:08 +0900

    [JPA Auditing] - 전체 게시글 목록 조회시 model에 전달할 dto 수정 , modifiedDate 필드

--- src/main/java/springbootweb/web/dto/PostsListResponseDto.java
+++ src/main/java/springbootweb/web/dto/PostsListResponseDto.java
@@ -16,5 +16,6 @@ public class PostsListResponseDto { // 전체 조회 Dto
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
     }
 }
