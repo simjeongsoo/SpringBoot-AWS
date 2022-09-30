2072a7e6ee57573de7f95c745a7caac6b7cc5d15
simjeongsoo <simking95@naver.com>
2022-09-30 19:57:25 +0900

    [JPA Auditing] - JPA Auditing 테스트 코드 작성

--- src/test/java/springbootweb/domain/posts/PostsRepositoryTest.java
+++ src/test/java/springbootweb/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,7 +29,7 @@ public class PostsRepositoryTest {
     }
 
     @Test
-    public void 게시글저장_불러오기() { // test passed 
+    public void 게시글저장_불러오기() { // test passed
         // given
         String title = "테스트 게시글";
         String content = "테스트 본문";
@@ -43,12 +44,34 @@ public class PostsRepositoryTest {
                 .build());
 
         //when
-        List<Posts> postsList = postsRepository.findAll(); // findAll() :  테이블 posts 에 있는 모든데이터 조회
+        List<Posts> postsList = postsRepository.findAll();// findAll() :  테이블 posts 에 있는 모든데이터 조회
 
         //then
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() throws Exception { // test passed
+        //given
+        LocalDateTime now = LocalDateTime.of(2022, 9, 30, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>> createDate=" + posts.getCreatedDate()
+                + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
 }
