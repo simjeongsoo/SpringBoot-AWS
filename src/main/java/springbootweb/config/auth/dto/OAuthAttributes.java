e542efc7e423960128f145b5684d25d8aa82073a
simjeongsoo <simking95@naver.com>
2022-10-04 19:01:08 +0900

    [네이버 로그인] - OAuthAttributes 클래스에 네이버인지 판단하는 코드와 네이버 생성자 추가

--- src/main/java/springbootweb/config/auth/dto/OAuthAttributes.java
+++ src/main/java/springbootweb/config/auth/dto/OAuthAttributes.java
@@ -26,6 +26,12 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+
+        //--네이버 로그인--//
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         //--OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환--//
         return ofGoogle(userNameAttributeName, attributes);
     }
@@ -36,6 +42,19 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
