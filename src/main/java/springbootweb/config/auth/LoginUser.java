55a50f9be0ce939bf893257e779696eab3f43741
simjeongsoo <simking95@naver.com>
2022-10-04 17:47:38 +0900

    [어노테이션 기반 반복 코드 개선] - @LoginUser 어노테이션 생성

---
+++ src/main/java/springbootweb/config/auth/LoginUser.java
@@ -0,0 +1,11 @@
+package springbootweb.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME) // 보존정책.런타임
+public @interface LoginUser {
+}
