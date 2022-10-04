76afb4cbf0860a7e8aa40a5e1fac5831045834a2
simjeongsoo <simking95@naver.com>
2022-10-04 16:55:24 +0900

    [OAuth2 구글 로그인 연동] - Controller, User 정보 전달

--- src/main/java/springbootweb/web/IndexController.java
+++ src/main/java/springbootweb/web/IndexController.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import springbootweb.config.auth.dto.SessionUser;
 import springbootweb.service.posts.PostsService;
 import springbootweb.web.dto.PostsResponseDto;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -14,15 +17,27 @@ public class IndexController {
     // 페이지 관련 컨트롤러 //
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
-    @GetMapping("/") // home view mapping
-    public String index(Model model) {
+    @GetMapping("/")
+    public String index(Model model) { // home view mapping
         model.addAttribute("posts", postsService.findAllDesc());
+
+        //--index.mustache 에서 userName을 사용할 수 있게 IndexController에서 userName을 model에 저장하는 코드--//
+
+        // CustomOAuth2UserService 에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
+        // 로그인 성공 시 httpSession.getAttribute(”user”)에서 값을 가져 올 수 있음
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            // 세션에 저장된 값이 있을때만 model에 userName으로 등록
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
     @GetMapping("/posts/save") // 게시글 등록 mapping
-    public String postSave() {
+    public String postSave(Model model) {
         return "posts-save";
     }
 
